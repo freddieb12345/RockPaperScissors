3f4e5748c7aec33caed8fc748a810edc29a86e6e
Freddie Brewin <freddiebrewin@live.com>
2021-02-27 14:58:03 +0000

    Adding increments to the tally variables to keep track of the score

--- script.js
+++ script.js
@@ -9,26 +9,39 @@ var computerChoice = computerChoiceArray[Math.floor(Math.random()*computerChoice
 window.alert("The computer chose " + computerChoice);
 
 //Compare the userInput to the computerChoice and decide who wins by setting rules.
+//Also need to create variables to tally how many wins draws and losses you have
+//Wins
+var Wins =0;
+var Draws =0;
+var Losses =0;
+
 if (userInput === "R" && computerChoice === "S") {
     window.alert("You win");
+    userWins ++;
 }
 if (userInput === "P" && computerChoice === "R") {
     window.alert("You win");
+    userWins ++;
 }
 if (userInput === "S" && computerChoice === "P") {
     window.alert("You win");
+    userWins ++;
 }
-
+//Losses
 if (userInput === "S" && computerChoice === "R") {
     window.alert("You lose");
+    userlosses ++;
 }
 if (userInput === "R" && computerChoice === "P") {
     window.alert("You lose");
+    userlosses ++;
 }
 if (userInput === "P" && computerChoice === "S") {
     window.alert("You lose");
+    userlosses ++;
 }
-
+//Draw
 if (userInput === computerChoice) {
     window.alert("Draw");
+    userDraws ++;
 }
