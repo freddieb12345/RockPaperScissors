17eed72e4e17beaa1df93dd4138f13fbe19cd24d
Freddie Brewin <freddiebrewin@live.com>
2021-02-27 14:49:08 +0000

    Added comparison between userInput and computerChoice that decides winner

--- script.js
+++ script.js
@@ -2,9 +2,33 @@
 var userInput = window.prompt("Enter R, P or S", userInput);
 console.log(userInput);
 
+//Creating an array and then creating a variable that is a random selection of the array, then alert that variable to the user.
+
 var computerChoiceArray = ["R","P","S"];
-
-//Create a variable that is a random selection of the above array, then alert that variable to the user.
-
 var computerChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)]
 window.alert("The computer chose " + computerChoice);
+
+//Compare the userInput to the computerChoice and decide who wins by setting rules.
+if (userInput === "R" && computerChoice === "S") {
+    window.alert("You win");
+}
+if (userInput === "P" && computerChoice === "R") {
+    window.alert("You win");
+}
+if (userInput === "S" && computerChoice === "P") {
+    window.alert("You win");
+}
+
+if (userInput === "S" && computerChoice === "R") {
+    window.alert("You lose");
+}
+if (userInput === "R" && computerChoice === "P") {
+    window.alert("You lose");
+}
+if (userInput === "P" && computerChoice === "S") {
+    window.alert("You lose");
+}
+
+if (userInput === computerChoice) {
+    window.alert("Draw");
+}
