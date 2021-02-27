cb36e0eba197a3d94d53a1c1e9a7e985bc4bfee4
Freddie Brewin <freddiebrewin@live.com>
2021-02-27 15:32:11 +0000

    Created loop that keeps the game going until cancelled

--- script.js
+++ script.js
@@ -30,15 +30,15 @@ if (userInput === "S" && computerChoice === "P") {
 //Losses
 if (userInput === "S" && computerChoice === "R") {
     window.alert("You lose");
-    userlosses ++;
+    userLosses ++;
 }
 if (userInput === "R" && computerChoice === "P") {
     window.alert("You lose");
-    userlosses ++;
+    userLosses ++;
 }
 if (userInput === "P" && computerChoice === "S") {
     window.alert("You lose");
-    userlosses ++;
+    userLosses ++;
 }
 //Draw
 if (userInput === computerChoice) {
@@ -47,4 +47,51 @@ if (userInput === computerChoice) {
 }
 
 //Alert that shows your stats with a button on it that resets all the initial variables (not the tally), allowing for the game to restart
-window.confirm ("Stats:\n" + "Wins = " + userWins + "\nDraws = " + userDraws  + "\nLosses = " + userLosses);
+window.confirm ("Stats:\n" + "Wins = " + userWins + "\nDraws = " + userDraws  + "\nLosses = " + userLosses + "\nPress ok to play again");
+
+//Creating condition that would loop the game back to the start if the user presses okay but would end the game if the user presses cancel
+reset = true;
+while (reset = true) {
+    if (confirm ("Stats:\n" + "Wins = " + userWins + "\nDraws = " + userDraws  + "\nLosses = " + userLosses + "\nPress ok to play again") === true){
+        computerChoice;
+        userInput;
+
+        userInput = window.prompt("Enter R, P or S", userInput);
+        computerChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)];
+        window.alert("The computer chose " + computerChoice);
+        if (userInput === "R" && computerChoice === "S") {
+            window.alert("You win");
+            userWins ++;
+        }
+        if (userInput === "P" && computerChoice === "R") {
+            window.alert("You win");
+            userWins ++;
+        }
+        if (userInput === "S" && computerChoice === "P") {
+            window.alert("You win");
+            userWins ++;
+        }
+        //Losses
+        if (userInput === "S" && computerChoice === "R") {
+            window.alert("You lose");
+            userLosses ++;
+        }
+        if (userInput === "R" && computerChoice === "P") {
+            window.alert("You lose");
+            userLosses ++;
+        }
+        if (userInput === "P" && computerChoice === "S") {
+            window.alert("You lose");
+            userLosses ++;
+        }
+        //Draw
+        if (userInput === computerChoice) {
+            window.alert("Draw");
+            userDraws ++;
+        }
+        window.confirm ("Stats:\n" + "Wins = " + userWins + "\nDraws = " + userDraws  + "\nLosses = " + userLosses + "\nPress ok to play again");
+    } else {
+        window.alert("Game Over");
+        reset = false;
+    }
+}
