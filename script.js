43df62d55b9dab3421955f33619d397e7969401c
Freddie Brewin <freddiebrewin@live.com>
2021-02-27 14:38:31 +0000

    Generated opponent input

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //Creating an alert that asks the user for an input of Rock, Paper or Scissors
 var userInput = window.prompt("Enter R, P or S", userInput);
+console.log(userInput);
 
-console.log(userInput)
+var computerChoiceArray = ["R","P","S"];
+
+//Create a variable that is a random selection of the above array, then alert that variable to the user.
+
+var computerChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)]
+window.alert("The computer chose " + computerChoice);
